75ca1553155e03d455c079335413f34a10a8e38b
Sudharssen Murali <m.sudharssen@gmail.com>
2025-08-06 00:07:03 -0400

    added .gitignore

--- obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7336e49f838a8cdbc8e89460027103c9e2116e87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77055054cfe67e106540d0e9e6198ed0a72758e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
